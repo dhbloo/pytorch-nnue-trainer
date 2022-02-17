aef1281a90d91ed101cc4718a1bd00fb4f53be64
dhbloo <duanhb1084@qq.com>
2022-02-17 22:59:51 +0800

    add resnet output head v1

--- model/resnet.py
+++ model/resnet.py
@@ -7,7 +7,7 @@ from .blocks import Conv2dBlock, LinearBlock, ResBlock
 def build_input_plane(input_type):
     if input_type == 'basic':
         return BasicInputPlane(with_stm=True)
-    elif input_type == 'basic_no_stm':
+    elif input_type == 'basic-nostm':
         return BasicInputPlane(with_stm=False)
     elif input_type == 'raw':
         return lambda x: x  # identity transform
@@ -24,6 +24,9 @@ def build_head(head_type, dim_feature):
 
     if head_type == 'v0':
         return OutputHeadV0(dim_feature, dim_value)
+    elif head_type == 'v1' or head_type.starts_with('v1-'):
+        scale = int(head_type[3:] or 1)
+        return OutputHeadV1(dim_feature, dim_feature * scale, dim_value)
     else:
         assert 0, f"Unsupported head: {head_type}"
 
@@ -67,6 +70,16 @@ class OutputHeadV0(nn.Module):
         policy = torch.squeeze(policy, dim=1)
 
         return value, policy
+
+
+class OutputHeadV1(OutputHeadV0):
+    def __init__(self, dim_feature, dim_middle, dim_value=3):
+        super().__init__(dim_middle, dim_value)
+        self.conv = Conv2dBlock(dim_feature, dim_middle, ks=3, st=1, padding=1)
+
+    def forward(self, feature):
+        feature = self.conv(feature)
+        return super().forward(feature)
 
 
 class ResNet(nn.Module):
