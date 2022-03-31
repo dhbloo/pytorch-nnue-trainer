964c9f7788d7eb85a0cd935cf0071e26b2859a8d
dhbloo <duanhb1084@qq.com>
2022-03-31 22:19:11 +0800

    add linear_threat model

--- model/linear.py
+++ model/linear.py
@@ -9,7 +9,7 @@ class LinearModel(nn.Module):
                  pcode_dim=2380,
                  has_draw_value=True,
                  two_side=False,
-                 fixed_side_input=False) -> None:
+                 fixed_side_input=False):
         super().__init__()
         self.pcode_dim = pcode_dim
         self.has_draw_value = has_draw_value
@@ -45,7 +45,7 @@ class LinearModel(nn.Module):
             win = value[:, 0, 0] - value[:, 1, 0]  # [B]
             loss = -win  # [B]
             draw = value[:, 0, 1] + value[:, 1, 1]  # [B]
-            value = torch.stack((win, loss, draw), dim=1)
+            value = torch.stack((win, loss, draw), dim=1)  # [B, 3]
         else:
             value = value[:, 0] - value[:, 1]  # [B, 1] no draw value
 
@@ -80,3 +80,56 @@ class LinearModel(nn.Module):
         if self.two_side: flags += '-twoside'
         if self.fixed_side_input: flags += '-fixed'
         return f"linear_{flags}"
+
+
+@MODELS.register('linear_threat')
+class LinearThreatModel(LinearModel):
+    def __init__(self, p4_dim=14, **kwargs):
+        super().__init__(**kwargs)
+        self.p4_dim = p4_dim
+        self.threat_table = nn.Embedding(num_embeddings=2048, embedding_dim=1)
+
+    def forward(self, data):
+        value, policy = super().forward(data)
+
+        assert torch.all(self.p4_dim == data['sparse_feature_dim'][:, 8:10])
+        p4_sparse_input = data['sparse_feature_input'][:, [8, 9]].int()  # [B, 2, H, W]
+
+        p4count = torch.zeros(len(p4_sparse_input), 2, self.p4_dim)  # [B, 2, p4_dim]
+        for p4 in range(1, self.p4_dim):
+            p4count[:, :, p4] = torch.count_nonzero(p4_sparse_input == p4, dim=(2, 3))
+
+        oppo_five = p4count[:, 1, 13] > 0
+        self_flexfour = p4count[:, 0, 12] > 0
+        oppo_flexfour = p4count[:, 1, 12] > 0
+        self_fourplus = p4count[:, 0, 10] + p4count[:, 0, 11] > 0
+        oppo_fourplus = p4count[:, 1, 10] + p4count[:, 1, 11] > 0
+        self_four = p4count[:, 0, 9] > 0
+        oppo_four = p4count[:, 1, 9] > 0
+        self_threeplus = p4count[:, 0, 7] + p4count[:, 0, 8] > 0
+        oppo_threeplus = p4count[:, 1, 7] + p4count[:, 1, 8] > 0
+        self_three = p4count[:, 0, 6] > 0
+        oppo_three = p4count[:, 1, 6] > 0
+
+        threat_mask = 0b1 & -oppo_five.int() \
+                    | 0b10 & -self_flexfour.int() \
+                    | 0b100 & -oppo_flexfour.int() \
+                    | 0b1000 & -self_fourplus.int() \
+                    | 0b10000 & -self_four.int() \
+                    | 0b100000 & -self_threeplus.int() \
+                    | 0b1000000 & -self_three.int() \
+                    | 0b10000000 & -oppo_fourplus.int() \
+                    | 0b100000000 & -oppo_four.int() \
+                    | 0b1000000000 & -oppo_threeplus.int() \
+                    | 0b10000000000 & -oppo_three.int()
+        threat_value = self.threat_table(threat_mask)  # [B, 1]
+        value = value[:, 0:1] + threat_value  # [B, 1 or 3]
+
+        return value, policy
+
+    @property
+    def name(self):
+        flags = 'draw' if self.has_draw_value else 'nodraw'
+        if self.two_side: flags += '-twoside'
+        if self.fixed_side_input: flags += '-fixed'
+        return f"linear_threat_{flags}"
